946a41a4d265b6e384be22d7d8d1968abaae6c58
LucaCode <lucalstyle@gmail.com>
2018-05-02 03:33:40 +0200

    change folder name to lib, remove temp folder

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ GitHub: LucaCode
 ©Copyright by Luca Scaringella
  */
 
-const ZationStarter   = require('./src/main/zationStarter');
+const ZationStarter   = require('./lib/main/zationStarter');
 
 //starter
 module.exports.start = (options) =>
@@ -13,11 +13,11 @@ module.exports.start = (options) =>
 };
 
 //Api Classes
-module.exports.Bag = require('./src/api/Bag');
-module.exports.SmallBag = require('./src/api/SmallBag');
-module.exports.Controller = require('./src/api/Controller');
-module.exports.Result = require('./src/api/Result');
-module.exports.TaskError = require('./src/api/TaskError');
-module.exports.TaskErrorBag = require('./src/api/TaskErrorBag');
+module.exports.Bag = require('./lib/api/Bag');
+module.exports.SmallBag = require('./lib/api/SmallBag');
+module.exports.Controller = require('./lib/api/Controller');
+module.exports.Result = require('./lib/api/Result');
+module.exports.TaskError = require('./lib/api/TaskError');
+module.exports.TaskErrorBag = require('./lib/api/TaskErrorBag');
 
-module.exports.ErrorType = require('./src/helper/constants/errorTypes');
+module.exports.ErrorType = require('./lib/helper/constants/errorTypes');
