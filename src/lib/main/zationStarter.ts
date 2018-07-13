195d9413efe54d83470b5b2ee4107d27c295af1e
Luca Scaringella <32013416+LucaCode@users.noreply.github.com>
2018-07-14 01:12:50 +0200

    change version for testing

--- src/lib/main/zationStarter.ts
+++ src/lib/main/zationStarter.ts
@@ -20,7 +20,7 @@ import BackgroundTasksSetter = require("../helper/background/backgroundTasksSett
 class ZationStarter
 {
     private static instance : ZationStarter | null = null;
-    private static readonly version : string = '0.6.5';
+    private static readonly version : string = '0.0.2';
 
     private readonly serverStartedTimeStamp : number;
     private readonly zc : ZationConfig;
