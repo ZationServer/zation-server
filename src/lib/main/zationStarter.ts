da48d40fcea904c6a9c39a66c8ce4701ffcceb49
LucaCode <lucalstyle@gmail.com>
2018-08-14 15:33:31 +0200

    new version 0.2.4

--- src/lib/main/zationStarter.ts
+++ src/lib/main/zationStarter.ts
@@ -22,7 +22,7 @@ const  isWindows             = require('is-windows');
 class ZationStarter
 {
     private static instance : ZationStarter | null = null;
-    private static readonly version : string = '0.2.3';
+    private static readonly version : string = '0.2.4';
 
     private readonly serverStartedTimeStamp : number;
     private readonly zc : ZationConfig;
