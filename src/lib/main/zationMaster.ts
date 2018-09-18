34a582203f2c37072dc905fb5fb066fd18d8f347
Luca Scaringella <lucalstyle@gmail.com>
2018-09-18 12:57:25 +0200

    update version 0.3.6

--- src/lib/main/zationMaster.ts
+++ src/lib/main/zationMaster.ts
@@ -22,7 +22,7 @@ import StateServerEngine     = require("../helper/cluster/stateServerEngine");
 class ZationMaster
 {
     private static instance : ZationMaster | null = null;
-    private static readonly version : string = '0.3.5';
+    private static readonly version : string = '0.3.6';
 
     private readonly serverStartedTimeStamp : number;
     private readonly zc : ZationConfig;
