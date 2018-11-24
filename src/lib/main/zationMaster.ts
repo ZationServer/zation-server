8ebc79c3ba816951e4f58839a05baacb40cb7a02
Luca Scaringella <lucalstyle@gmail.com>
2018-11-24 18:11:16 +0100

    update to version 0.5.1 and client to 0.3.7

--- src/lib/main/zationMaster.ts
+++ src/lib/main/zationMaster.ts
@@ -23,7 +23,7 @@ import {WorkerMessageActions} from "../helper/constants/workerMessageActions";
 
 class ZationMaster {
     private static instance: ZationMaster | null = null;
-    private static readonly version: string = '0.5.0';
+    private static readonly version: string = '0.5.1';
 
     private readonly serverStartedTimeStamp: number;
     private readonly zc: ZationConfig;
