6dd2820e3c3cf8b1c47aa2a65475d5b8f6f68c74
Luca Scaringella <lucalstyle@gmail.com>
2019-01-11 19:12:32 +0100

    add new tests for validation functions

--- test/src/helper/validator/functions.test.js
+++ test/src/helper/validator/functions.test.js
@@ -1,9 +1,11 @@
 const assert          = require("chai").assert;
 const TaskErrorBag    = require('../../../../dist/lib/api/TaskErrorBag');
 const funcValidator   = require('../../../../dist/lib/helper/validator/validatorLibrary').function;
+const validationTypes = require('../../../../dist/lib/helper/constants/validationTypes').ValidationTypes;
 const FormatLetters   = require('../../../../dist/lib/helper/constants/validation').FormatLetters;
 const ConfigPreCompiler = require('../../../../dist/lib/helper/config/configPreCompiler');
 const ZationConfigStub  = require('./../../../stubs/zationConfig');
+const Base64TestData    = require('./../../../testData/base64');
 
 describe('Function Validation',() => {
 
@@ -146,22 +148,46 @@ describe('Function Validation',() => {
 
     describe('Contains',() => {
 
-        it('Not matching input should produce an error',() => {
-            const eb = new TaskErrorBag;
-            funcValidator.contains('hi luca','hello',eb,{});
-            assert(!eb.isEmpty());
+        describe('Contains Normal',() => {
+
+            it('Not matching input should produce an error',() => {
+                const eb = new TaskErrorBag;
+                funcValidator.contains('hi luca','hello',eb,{});
+                assert(!eb.isEmpty());
+            });
+
+            it('Matching should produce no error',() => {
+                const eb = new TaskErrorBag;
+                funcValidator.contains('mh, hello luca','hello',eb,{});
+                assert(eb.isEmpty());
+            });
+
+            it('Not a string should produce no error',() => {
+                const eb = new TaskErrorBag;
+                funcValidator.contains(5,'hello',eb,{});
+                assert(eb.isEmpty());
+            });
         });
 
-        it('Matching should produce no error',() => {
-            const eb = new TaskErrorBag;
-            funcValidator.contains('mh, hello luca','hello',eb,{});
-            assert(eb.isEmpty());
-        });
+        describe('Contains Array',() => {
 
-        it('Not a string should produce no error',() => {
-            const eb = new TaskErrorBag;
-            funcValidator.contains(5,'hello',eb,{});
-            assert(eb.isEmpty());
+            it('Not matching input should produce an error',() => {
+                const eb = new TaskErrorBag;
+                funcValidator.contains('hi luca',['hello','hi'],eb,{});
+                assert(!eb.isEmpty());
+            });
+
+            it('Matching should produce no error',() => {
+                const eb = new TaskErrorBag;
+                funcValidator.contains('mh, hello luca',['luca','mh'],eb,{});
+                assert(eb.isEmpty());
+            });
+
+            it('Not a string should produce no error',() => {
+                const eb = new TaskErrorBag;
+                funcValidator.contains(5,['hello','mh'],eb,{});
+                assert(eb.isEmpty());
+            });
         });
 
     });
@@ -332,6 +358,216 @@ describe('Function Validation',() => {
             const eb = new TaskErrorBag;
             funcValidator.charClass(5,config.charClass,eb,{});
             assert(eb.isEmpty());
+        });
+    });
+
+    describe('Before',() => {
+
+        it('Not matching input should produce an error',() => {
+            const eb = new TaskErrorBag;
+            funcValidator.before('04/12/2017',Date.parse('03/12/2016'),eb,{});
+            assert(!eb.isEmpty());
+        });
+
+        it('Matching should produce no error',() => {
+            const eb = new TaskErrorBag;
+            funcValidator.before('02/11/2015',Date.parse('03/12/2016'),eb,{});
+            assert(eb.isEmpty());
+        });
+
+        it('A number should produce no error',() => {
+            const eb = new TaskErrorBag;
+            funcValidator.before(5,Date.parse('10/03/2016'),eb,{});
+            assert(eb.isEmpty());
+        });
+    });
+
+    describe('After',() => {
+
+        it('Not matching input should produce an error',() => {
+            const eb = new TaskErrorBag;
+            funcValidator.after('04/14/2016',Date.parse('03/18/2019'),eb,{});
+            assert(!eb.isEmpty());
+        });
+
+        it('Matching should produce no error',() => {
+            const eb = new TaskErrorBag;
+            funcValidator.after('12/11/2019',Date.parse('03/12/2016'),eb,{});
+            assert(eb.isEmpty());
+        });
+
+        it('A number should produce no error',() => {
+            const eb = new TaskErrorBag;
+            funcValidator.after(5,Date.parse('10/03/2016'),eb,{});
+            assert(eb.isEmpty());
+        });
+    });
+
+    describe('MinByteSize',() => {
+
+        describe('String',() => {
+            it('Not matching input should produce an error',() => {
+                const eb = new TaskErrorBag;
+                funcValidator.minByteSize('a',40,eb,{});
+                assert(!eb.isEmpty());
+            });
+
+            it('Matching should produce no error',() => {
+                const eb = new TaskErrorBag;
+                funcValidator.minByteSize('hello my name is tom',10,eb,{});
+                assert(eb.isEmpty());
+            });
+        });
+
+        describe('Base64',() => {
+
+            it('Not matching input should produce an error',() => {
+                const eb = new TaskErrorBag;
+                funcValidator.minByteSize(Base64TestData.d1.data,Base64TestData.d1.fileByte*2,eb,{},undefined,validationTypes.BASE64);
+                assert(!eb.isEmpty());
+            });
+
+            it('Matching should produce no error',() => {
+                const eb = new TaskErrorBag;
+                funcValidator.minByteSize(Base64TestData.d1.data,Base64TestData.d1.fileByte,eb,{},undefined,validationTypes.BASE64);
+                assert(eb.isEmpty());
+            });
+        });
+
+        it('A number should produce no error',() => {
+            const eb = new TaskErrorBag;
+            funcValidator.minByteSize(5,10,eb,{});
+            assert(eb.isEmpty());
+        });
+    });
+
+    describe('MaxByteSize',() => {
+
+        describe('String',() => {
+            it('Not matching input should produce an error',() => {
+                const eb = new TaskErrorBag;
+                funcValidator.maxByteSize('a',0,eb,{});
+                assert(!eb.isEmpty());
+            });
+
+            it('Matching should produce no error',() => {
+                const eb = new TaskErrorBag;
+                funcValidator.maxByteSize('hello my name is tom',200,eb,{});
+                assert(eb.isEmpty());
+            });
+        });
+
+        describe('Base64',() => {
+
+            it('Not matching input should produce an error',() => {
+                const eb = new TaskErrorBag;
+                funcValidator.maxByteSize(Base64TestData.d1.data,Base64TestData.d1.fileByte,eb,{},undefined,validationTypes.BASE64);
+                assert(!eb.isEmpty());
+            });
+
+            it('Matching should produce no error',() => {
+                const eb = new TaskErrorBag;
+                funcValidator.maxByteSize(Base64TestData.d1.data,Base64TestData.d1.fileByte*2,eb,{},undefined,validationTypes.BASE64);
+                assert(eb.isEmpty());
+            });
+        });
+
+        it('A number should produce no error',() => {
+            const eb = new TaskErrorBag;
+            funcValidator.maxByteSize(5,10,eb,{});
+            assert(eb.isEmpty());
+        });
+    });
+
+    describe('MimeType',() => {
+
+        describe('MimeType Normal',() => {
+
+            it('Not matching input should produce an error (string)',() => {
+                const eb = new TaskErrorBag;
+                funcValidator.mimeType(Base64TestData.d2.data,'text',eb,{});
+                assert(!eb.isEmpty());
+            });
+
+            it('Matching should produce no error (string)',() => {
+                const eb = new TaskErrorBag;
+                funcValidator.mimeType(Base64TestData.d2.data,Base64TestData.d2.mimeType,eb,{});
+                assert(eb.isEmpty());
+            });
+
+            it('A number should produce no error',() => {
+                const eb = new TaskErrorBag;
+                funcValidator.mimeType(5,'image',eb,{});
+                assert(eb.isEmpty());
+            });
+        });
+
+        describe('MimeType Array',() => {
+
+            it('Not matching input should produce an error (array)',() => {
+                const eb = new TaskErrorBag;
+                funcValidator.mimeType(Base64TestData.d2.data,['text','audio'],eb,{});
+                assert(!eb.isEmpty());
+            });
+
+
+            it('Matching should produce no error (array)',() => {
+                const eb = new TaskErrorBag;
+                funcValidator.mimeType(Base64TestData.d2.data,[Base64TestData.d2.mimeType,'audio'],eb,{});
+                assert(eb.isEmpty());
+            });
+
+            it('A number should produce no error',() => {
+                const eb = new TaskErrorBag;
+                funcValidator.mimeType(5,['image','audio'],eb,{});
+                assert(eb.isEmpty());
+            });
+        });
+    });
+
+    describe('SubType',() => {
+
+        describe('SubType Normal',() => {
+
+            it('Not matching input should produce an error (string)',() => {
+                const eb = new TaskErrorBag;
+                funcValidator.subType(Base64TestData.d2.data,'jpg',eb,{});
+                assert(!eb.isEmpty());
+            });
+
+            it('Matching should produce no error (string)',() => {
+                const eb = new TaskErrorBag;
+                funcValidator.subType(Base64TestData.d2.data,Base64TestData.d2.subType,eb,{});
+                assert(eb.isEmpty());
+            });
+
+            it('A number should produce no error',() => {
+                const eb = new TaskErrorBag;
+                funcValidator.subType(5,'image',eb,{});
+                assert(eb.isEmpty());
+            });
+        });
+
+        describe('SubType Array',() => {
+
+            it('Not matching input should produce an error (array)',() => {
+                const eb = new TaskErrorBag;
+                funcValidator.subType(Base64TestData.d2.data,['jpg','svg'],eb,{});
+                assert(!eb.isEmpty());
+            });
+
+
+            it('Matching should produce no error (array)',() => {
+                const eb = new TaskErrorBag;
+                funcValidator.subType(Base64TestData.d2.data,[Base64TestData.d2.subType,'jpg'],eb,{});
+                assert(eb.isEmpty());
+            });
+
+            it('A number should produce no error',() => {
+                const eb = new TaskErrorBag;
+                funcValidator.subType(5,['jpg','png'],eb,{});
+                assert(eb.isEmpty());
+            });
         });
     });
 
