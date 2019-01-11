43bc596ab4943af704be89c78d2c64b3b12f77da
Luca Scaringella <lucalstyle@gmail.com>
2019-01-11 01:04:07 +0100

    add tests

---
+++ test/helper/validation/types.test.js
@@ -0,0 +1,79 @@
+const assert = require('assert');
+const TaskErrorBag    = require('../../../dist/lib/api/TaskErrorBag');
+const typeValidator   = require('../../../dist/lib/helper/validator/validatorLibrary').type;
+const validationTypes = require('../../../dist/lib/helper/constants/validationTypes').ValidationTypes;
+describe('Type Validation',() => {
+
+    describe('Object',() => {
+        it('Object should produce no error',() => {
+            const eb = new TaskErrorBag;
+            typeValidator[validationTypes.OBJECT]({},eb,{});
+            assert(eb.isEmpty());
+        });
+
+        it('Number should produce an error',() => {
+            const eb = new TaskErrorBag;
+            typeValidator[validationTypes.OBJECT](1,eb,{});
+            assert(!eb.isEmpty());
+        });
+
+        it('Null should produce an error',() => {
+            const eb = new TaskErrorBag;
+            typeValidator[validationTypes.OBJECT](null,eb,{});
+            assert(!eb.isEmpty());
+        });
+
+        it('String should produce an error',() => {
+            const eb = new TaskErrorBag;
+            typeValidator[validationTypes.OBJECT]('hallo',eb,{});
+            assert(!eb.isEmpty());
+        });
+    });
+
+    describe('Array',() => {
+        it('Array should produce no error',() => {
+            const eb = new TaskErrorBag;
+            typeValidator[validationTypes.ARRAY]([],eb,{});
+            assert(eb.isEmpty());
+        });
+
+        it('Number should produce an error',() => {
+            const eb = new TaskErrorBag;
+            typeValidator[validationTypes.ARRAY](1,eb,{});
+            assert(!eb.isEmpty());
+        });
+
+        it('String should produce an error',() => {
+            const eb = new TaskErrorBag;
+            typeValidator[validationTypes.ARRAY]('hallo',eb,{});
+            assert(!eb.isEmpty());
+        });
+
+        it('Object should produce an error',() => {
+            const eb = new TaskErrorBag;
+            typeValidator[validationTypes.ARRAY]({},eb,{});
+            assert(!eb.isEmpty());
+        });
+    });
+
+    describe('String',() => {
+        it('String should produce no error',() => {
+            const eb = new TaskErrorBag;
+            typeValidator[validationTypes.STRING]('hallo',eb,{});
+            assert(eb.isEmpty());
+        });
+
+        it('Number should produce an error',() => {
+            const eb = new TaskErrorBag;
+            typeValidator[validationTypes.STRING](1,eb,{});
+            assert(!eb.isEmpty());
+        });
+
+        it('Object should produce an error',() => {
+            const eb = new TaskErrorBag;
+            typeValidator[validationTypes.STRING]({},eb,{});
+            assert(!eb.isEmpty());
+        });
+    })
+
+});
