981bcf916403bb3f55a497412366820c7311db77
Luca Scaringella <lucalstyle@gmail.com>
2019-02-01 22:37:00 +0100

    update zation-client version and zation-server version to 0.6.8

--- src/lib/main/zationMaster.ts
+++ src/lib/main/zationMaster.ts
@@ -24,7 +24,7 @@ import {StarterConfig}         from "../helper/configs/starterConfig";
 
 class ZationMaster {
     private static instance: ZationMaster | null = null;
-    private static readonly version: string = '0.6.7';
+    private static readonly version: string = '0.6.8';
 
     private readonly serverStartedTimeStamp: number;
     private readonly zc: ZationConfig;
