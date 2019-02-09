1a67ca2f3251d1e6cd127902396d0f58e2c58580
Luca Scaringella <lucalstyle@gmail.com>
2019-02-09 17:12:45 +0100

    update to version 0.7.0

--- src/lib/main/zationMaster.ts
+++ src/lib/main/zationMaster.ts
@@ -24,7 +24,7 @@ import {StarterConfig}         from "../helper/configs/starterConfig";
 
 class ZationMaster {
     private static instance: ZationMaster | null = null;
-    private static readonly version: string = '0.6.9';
+    private static readonly version: string = '0.7.0';
 
     private readonly serverStartedTimeStamp: number;
     private readonly zc: ZationConfig;
