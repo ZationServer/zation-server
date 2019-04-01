c21230f52d3603fd7abf607fde3b8beb0090f767
Luca Scaringella <lucalstyle@gmail.com>
2019-04-01 23:29:06 +0200

    add JsDoc for backgroundTasks and anyOf

--- src/lib/helper/configs/appConfig.ts
+++ src/lib/helper/configs/appConfig.ts
@@ -127,6 +127,20 @@ export type Property = ValuePropertyConfig | ObjectPropertyConfig | ArrayPropert
 
 export interface AnyOfProperty extends PropertyOptional
 {
+    /**
+     * With the anyOf modifier, you can define different properties.
+     * The input needs only to match with one of them to be valid.
+     * @example
+     * ```
+     * // any of with array
+     * anyOf : ['v.email','v.userName'],
+     * // any of with object (Will help to get a better input path)
+     * anyOf : {
+     *   email : 'v.email',
+     *   userName : 'v.userName'
+     * }
+     * ```
+     */
     anyOf : Record<string,Property> | Property[]
 }
 
@@ -134,8 +148,44 @@ export type TaskFunction = (smallBag : SmallBag) => Promise<void> | void;
 
 export interface BackgroundTask
 {
+    /**
+     * Defines when the background task should be invoked.
+     * At will invoke the background task only one time.
+     * You can use a number to define the milliseconds or a TimeObj to define a Time.
+     * @example
+     * // Will invoke the background task when the hour is 10.
+     * at : {hour : 10}
+     * // Will invoke the background task when the hour is 10 or 8.
+     * at : [{hour : 10},{hour : 8}]
+     * // Will invoke the background task when the hour is 10 and second 30.
+     * at : {hour : 10,second : 30}
+     * // Will invoke the background task after 30 seconds.
+     * at : 30000
+     */
     at  ?: number | TimeObj | TimeObj[] | number[];
+    /**
+     * Defines when the background task should be invoked.
+     * Every will invoke the background task every time.
+     * You can use a number to define the milliseconds or a TimeObj to define a Time.
+     * @example
+     * // Will invoke the background task whenever the hour is 10.
+     * every : {hour : 10}
+     * // Will invoke the background task whenever the hour is 10 or 8.
+     * every : [{hour : 10},{hour : 8}]
+     * // Will invoke the background task whenever the hour is 10 and second 30.
+     * every : {hour : 10,second : 30}
+     * // Will invoke the background task every 30 seconds.
+     * every : 30000
+     */
     every  ?: number | TimeObj | TimeObj[] | number[];
+    /**
+     * The task method defines the general task of the background task.
+     * Optionally you can pass an array of tasks.
+     * @example
+     * task : (sb : SmallBag) => {
+     *    console.log(`TaskRunning on worker -> ${sb.getWorkerId()}`);
+     * },
+     */
     task  ?: TaskFunction | TaskFunction[];
 }
 
