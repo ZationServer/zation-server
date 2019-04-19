124ab211bafce3e50a0b77091379f4c4d95af968
Luca Scaringella <lucalstyle@gmail.com>
2019-04-19 20:42:27 +0200

    fix test renaming

--- test/stubs/zationConfig.js
+++ test/stubs/zationConfig.js
@@ -12,7 +12,6 @@ const zcStub = sinon.createStubInstance(ZationConfig);
 sinon.stub(zcStub,"appConfig").value({});
 sinon.stub(zcStub,"channelConfig").value({});
 sinon.stub(zcStub,"mainConfig").value({});
-sinon.stub(zcStub,"errorConfig").value({});
 sinon.stub(zcStub,"eventConfig").value({});
 sinon.stub(zcStub,"serviceConfig").value({});
 sinon.stub(zcStub,"starterConfig").value({});
