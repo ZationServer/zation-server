7f4c976508709c5d9a3402184702978a3c79c1fd
Luca Scaringella <lucalstyle@gmail.com>
2019-05-15 18:28:32 +0200

    add jsDoc eventConfig

--- src/lib/helper/configDefinitions/eventConfig.ts
+++ src/lib/helper/configDefinitions/eventConfig.ts
@@ -7,22 +7,14 @@ GitHub: LucaCode
 import ExpressCore                   = require("express-serve-static-core");
 import ZationWorker                  = require("../../main/zationWorker");
 import ScServer                        from "../sc/scServer";
-import UpSocket, {HandshakeSocket} from "../sc/socket";
+import UpSocket, {HandshakeSocket}     from "../sc/socket";
 import {ZationToken}                   from "../constants/internal";
 import BackError                       from "../../api/BackError";
 import BackErrorBag                    from "../../api/BackErrorBag";
 import SmallBag                        from "../../api/SmallBag";
 import ZationInfo                      from "../infoObjects/zationInfo";
-import {
-    AuthMiddlewareReq,
-    EmitMiddlewareReq,
-    HandshakeScMiddlewareReq, HandshakeWsMiddlewareReq,
-    PubInMiddlewareReq,
-    PubOutMiddlewareReq,
-    SubMiddlewareReq
-} from "../sc/scMiddlewareReq";
-import SocketInfo from "../infoObjects/socketInfo";
-import CodeError  from "../error/codeError";
+import SocketInfo                      from "../infoObjects/socketInfo";
+import CodeError                       from "../error/codeError";
 
 export type ExpressFunction = (smallBag : SmallBag, express : ExpressCore.Express) => Promise<void> | void;
 export type ScServerFunction = (smallBag : SmallBag, scServer : ScServer) => Promise<void> | void;
@@ -173,42 +165,202 @@ export interface EventConfig
     middlewareSocket ?: MiddlewareSocketFunction;
 
     //socket cluster events
+    /**
+     * This gets triggered when an error occurs on a socket.
+     * This is a wrapper around a socket cluster event.
+     * Use it only for advanced use cases.
+     * @example (smallBag,socket,error) => {}
+     */
     sc_socketError  ?: SocketErrorFunction | SocketErrorFunction[];
+    /**
+     * This gets triggered whenever a client socket on the other side calls socket.send(...).
+     * This is a wrapper around a socket cluster event.
+     * Use it only for advanced use cases.
+     * @example (smallBag,socket,data) => {}
+     */
     sc_socketRaw  ?: SocketRawFunction | SocketRawFunction[];
+    /**
+     * Triggers when a socket completes the SC handshake phase and is fully connected.
+     * This is a wrapper around a socket cluster event.
+     * Use it only for advanced use cases.
+     * @example (smallBag,socket,conState) => {}
+     */
     sc_socketConnect  ?: SocketConnectFunction | SocketConnectFunction[];
+    /**
+     * Happens when a client becomes disconnected from the server.
+     * Note that if the socket becomes disconnected during the SC handshake stage,
+     * then the 'connectAbort' event will be triggered instead.
+     * This is a wrapper around a socket cluster event.
+     * Use it only for advanced use cases.
+     * @example (smallBag,socket,code,data) => {}
+     */
     sc_socketDisconnect  ?: SocketCodeDataFunction | SocketCodeDataFunction[];
+    /**
+     * Happens when a client disconnects from the server before the SocketCluster handshake has completed.
+     * This is a wrapper around a socket cluster event.
+     * Use it only for advanced use cases.
+     * @example (smallBag,socket,code,data) => {}
+     */
     sc_socketConnectAbort  ?: SocketCodeDataFunction | SocketCodeDataFunction[];
+    /**
+     * Happens when a client disconnects from the server at any stage of the handshake/connection cycle.
+     * This is a wrapper around a socket cluster event.
+     * Use it only for advanced use cases.
+     * @example (smallBag,socket,code,data) => {}
+     */
     sc_socketClose  ?: SocketCodeDataFunction | SocketCodeDataFunction[];
+    /**
+     * Emitted when a matching client socket successfully subscribes to a channel.
+     * This is a wrapper around a socket cluster event.
+     * Use it only for advanced use cases.
+     * @example (smallBag,socket,channelName,channelOptions) => {}
+     */
     sc_socketSubscribe  ?: SocketSubscribeFunction | SocketSubscribeFunction[];
+    /**
+     * Occurs whenever a matching client socket unsubscribes from a channel.
+     * This includes automatic unsubscriptions triggered by disconnects.
+     * This is a wrapper around a socket cluster event.
+     * Use it only for advanced use cases.
+     * @example (smallBag,socket,channelName) => {}
+     */
     sc_socketUnsubscribe  ?: SocketUnsubscribeFunction | SocketUnsubscribeFunction[];
+    /**
+     * Emitted when a client tries to authenticate itself with an invalid (or expired) token.
+     * This is a wrapper around a socket cluster event.
+     * Use it only for advanced use cases.
+     * @example (smallBag,socket,badAuthStatus,signedAuthToken) => {}
+     */
     sc_socketBadAuthToken  ?: SocketBadAuthTokenFunction | SocketBadAuthTokenFunction[];
+    /**
+     * Triggers whenever a client becomes authenticated.
+     * This is a wrapper around a socket cluster event.
+     * Use it only for advanced use cases.
+     * @example (smallBag,socket,code,authToken) => {}
+     */
     sc_socketAuthenticate  ?: SocketAuthenticateFunction | SocketAuthenticateFunction[];
+    /**
+     * Triggers whenever a client becomes unauthenticated.
+     * This is a wrapper around a socket cluster event.
+     * Use it only for advanced use cases.
+     * @example (smallBag,socket,oldAuthToken) => {}
+     */
     sc_socketDeauthenticate  ?: SocketDeauthenticateFunction | SocketDeauthenticateFunction[];
+    /**
+     * Triggers whenever a socket's authState changes
+     * (e.g., transitions between authenticated and unauthenticated states).
+     * This is a wrapper around a socket cluster event.
+     * Use it only for advanced use cases.
+     * @example (smallBag,socket,stateChangeData) => {}
+     */
     sc_socketAuthStateChange  ?: SocketAuthStateChangeFunction | SocketAuthStateChangeFunction[];
+    /**
+     * All data that arrives on a socket is emitted through this event as a string.
+     * This is a wrapper around a socket cluster event.
+     * Use it only for advanced use cases.
+     * @example (smallBag,socket,message) => {}
+     */
     sc_socketMessage  ?: SocketMessageFunction | SocketMessageFunction[];
 
+    /**
+     * This gets triggered when a fatal error occurs on a worker.
+     * This is a wrapper around a socket cluster event.
+     * Use it only for advanced use cases.
+     * @example (smallBag,error) => {}
+     */
     sc_serverError  ?: ScServerErrorFunction | ScServerErrorFunction[];
+    /**
+     * A notice carries potentially useful information but isn't quite an error.
+     * This is a wrapper around a socket cluster event.
+     * Use it only for advanced use cases.
+     * @example (smallBag,note) => {}
+     */
     sc_serverNotice  ?: ScServerNoticeFunction | ScServerNoticeFunction[];
+    /**
+     * It is emitted as soon as a new socket object is created on the server.
+     * This is a wrapper around a socket cluster event.
+     * Use it only for advanced use cases.
+     * @example (smallBag,socket) => {}
+     */
     sc_serverHandshake  ?: ScServerSocketFunction | ScServerSocketFunction[];
+    /**
+     * Emitted whenever a socket becomes disconnected during the handshake phase.
+     * This is a wrapper around a socket cluster event.
+     * Use it only for advanced use cases.
+     * @example (smallBag,socket,code,data) => {}
+     */
     sc_serverConnectionAbort  ?: ScServerSocketCodeDataFunction | ScServerSocketCodeDataFunction[];
+    /**
+     * Emitted whenever a connected socket becomes disconnected (after the handshake phase).
+     * This is a wrapper around a socket cluster event.
+     * Use it only for advanced use cases.
+     * @example (smallBag,socket,code,data) => {}
+     */
     sc_serverDisconnection  ?: ScServerSocketCodeDataFunction | ScServerSocketCodeDataFunction[];
+    /**
+     * Emitted whenever a connected socket becomes disconnected (at any stage of the handshake/connection cycle).
+     * This is a wrapper around a socket cluster event.
+     * Use it only for advanced use cases.
+     * @example (smallBag,socket,code,data) => {}
+     */
     sc_serverClosure  ?: ScServerSocketCodeDataFunction | ScServerSocketCodeDataFunction[];
+    /**
+     * Emitted whenever a new socket connection is established with the server (and the handshake has completed).
+     * This is a wrapper around a socket cluster event.
+     * Use it only for advanced use cases.
+     * @example (smallBag,socket,conState) => {}
+     */
     sc_serverConnection  ?: ScServerConnectionFunction | ScServerConnectionFunction[];
+    /**
+     * Emitted whenever a socket connection which is attached to the server becomes subscribed to a channel.
+     * This is a wrapper around a socket cluster event.
+     * Use it only for advanced use cases.
+     * @example (smallBag,socket,channelName,channelOptions) => {}
+     */
     sc_serverSubscription  ?: ScServerSubscriptionFunction | ScServerSubscriptionFunction[];
+    /**
+     * Emitted whenever a socket connection which is attached to the server becomes unsubscribed from a channel.
+     * This is a wrapper around a socket cluster event.
+     * Use it only for advanced use cases.
+     * @example (smallBag,socket,channelName) => {}
+     */
     sc_serverUnsubscription  ?: ScServerUnsubscriptionFunction | ScServerUnsubscriptionFunction[];
+    /**
+     * Emitted whenever a socket connection which is attached to the server becomes authenticated.
+     * This is a wrapper around a socket cluster event.
+     * Use it only for advanced use cases.
+     * @example (smallBag,socket,authToken) => {}
+     */
     sc_serverAuthentication  ?: ScServerAuthenticationFunction | ScServerAuthenticationFunction[];
+    /**
+     * Emitted whenever a socket connection which is attached to the server becomes deauthenticated.
+     * This is a wrapper around a socket cluster event.
+     * Use it only for advanced use cases.
+     * @example (smallBag,socket,oldAuthToken) => {}
+     */
     sc_serverDeauthentication  ?: ScServerDeauthenticationFunction | ScServerDeauthenticationFunction[];
+    /**
+     * Triggers whenever the authState of a socket which is attached to the server changes
+     * (e.g., transitions between authenticated and unauthenticated states).
+     * This is a wrapper around a socket cluster event.
+     * Use it only for advanced use cases.
+     * @example (smallBag,socket,stateChangeData) => {}
+     */
     sc_serverAuthenticationStateChange  ?: ScServerAuthenticationStateChangeFunction | ScServerAuthenticationStateChangeFunction[];
+    /**
+     * Emitted when a client which is attached to the server tries
+     * to authenticate itself with an invalid (or expired) token.
+     * This is a wrapper around a socket cluster event.
+     * Use it only for advanced use cases.
+     * @example (smallBag,socket,badAuthStatus) => {}
+     */
     sc_serverBadSocketAuthToken  ?: ScServerBadSocketAuthTokenFunction | ScServerBadSocketAuthTokenFunction[];
+    /**
+     * Emitted when the server is ready to accept connections.
+     * This is a wrapper around a socket cluster event.
+     * Use it only for advanced use cases.
+     * @example (smallBag) => {}
+     */
     sc_serverReady  ?: ScServerReadyFunction | ScServerReadyFunction[];
-
-    sc_middlewareAuthenticate  ?: ScMiddlewareFunction<AuthMiddlewareReq>;
-    sc_middlewareHandshakeWs  ?: ScMiddlewareFunction<HandshakeWsMiddlewareReq>;
-    sc_middlewareHandshakeSc  ?: ScMiddlewareFunction<HandshakeScMiddlewareReq>;
-    sc_middlewareSubscribe  ?: ScMiddlewareFunction<SubMiddlewareReq>;
-    sc_middlewarePublishIn  ?: ScMiddlewareFunction<PubInMiddlewareReq>;
-    sc_middlewarePublishOut  ?: ScMiddlewareFunction<PubOutMiddlewareReq>;
-    sc_middlewareEmit  ?: ScMiddlewareFunction<EmitMiddlewareReq>;
 }
 
 export interface PreCompiledEventConfig extends EventConfig
@@ -263,12 +415,4 @@ export interface PreCompiledEventConfig extends EventConfig
     sc_serverAuthenticationStateChange  : ScServerAuthenticationStateChangeFunction;
     sc_serverBadSocketAuthToken  : ScServerBadSocketAuthTokenFunction;
     sc_serverReady  : ScServerReadyFunction;
-
-    sc_middlewareAuthenticate  ?: ScMiddlewareFunction<AuthMiddlewareReq>;
-    sc_middlewareHandshakeWs  ?: ScMiddlewareFunction<HandshakeWsMiddlewareReq>;
-    sc_middlewareHandshakeSc  ?: ScMiddlewareFunction<HandshakeScMiddlewareReq>;
-    sc_middlewareSubscribe  ?: ScMiddlewareFunction<SubMiddlewareReq>;
-    sc_middlewarePublishIn  ?: ScMiddlewareFunction<PubInMiddlewareReq>;
-    sc_middlewarePublishOut  ?: ScMiddlewareFunction<PubOutMiddlewareReq>;
-    sc_middlewareEmit  ?: ScMiddlewareFunction<EmitMiddlewareReq>;
 }
