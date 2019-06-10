7db721392589b740e9950d1ca1cfdb6e1a4dcb71
Luca Scaringella <lucalstyle@gmail.com>
2019-06-10 22:11:54 +0200

    Implemented new param based class decorator.

---
+++ src/lib/api/decorator/ParamInput.ts
@@ -0,0 +1,26 @@
+/*
+Author: Luca Scaringella
+GitHub: LucaCode
+©Copyright by Luca Scaringella
+ */
+
+import {InputConfigTranslatable} from "../ConfigTranslatable";
+import {ParamInput as ParamInputConfig} from "../../helper/configDefinitions/appConfig";
+
+/**
+ * A class decorator that will mark the class as a param based input config.
+ * That means you can use the class as an input config.
+ * @constructor
+ */
+export const ParamInput = () => {
+    return (target : any) => {
+        const prototype = target.prototype;
+
+        const paramInput : ParamInputConfig =
+            typeof prototype['___models___'] === 'object' ? prototype['___models___'] : {};
+
+        (target as InputConfigTranslatable).__toInputConfig = () => {
+            return paramInput;
+        };
+    }
+};
