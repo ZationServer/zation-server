4219a37fd8cc0fd36d3204d31533b46b25a92211
Luca Scaringella <lucalstyle@gmail.com>
2019-06-17 11:05:20 +0200

    Updated to version 1.1.0

--- src/lib/main/zationMaster.ts
+++ src/lib/main/zationMaster.ts
@@ -28,7 +28,7 @@ import ConfigBuildError from "../helper/configManager/configBuildError";
 
 export default class ZationMaster {
     private static instance: ZationMaster | null = null;
-    private static readonly version: string = '1.0.9';
+    private static readonly version: string = '1.1.0';
 
     private readonly serverStartedTimeStamp: number;
     private zc: ZationConfigMaster;
