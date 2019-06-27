281b972145982e2029a02ac60ff821e24f474987
Luca Scaringella <lucalstyle@gmail.com>
2019-06-27 21:09:55 +0200

    Added doc for controller class.

--- src/lib/api/Controller.ts
+++ src/lib/api/Controller.ts
@@ -10,6 +10,18 @@ import {ControllerConfig} from "../helper/configDefinitions/controllerConfig";
 import SmallBag           from "./SmallBag";
 import BackErrorBag       from "./BackErrorBag";
 
+/**
+ * The controller is one of the main concepts of zation.
+ * It followers the request-response principle.
+ * The controller can be protected with lots of possibilities,
+ * also it supports input validation by using defined models.
+ * Additionally it is easy to return a result or a collection of errors to the client.
+ * A controller should be used for determining an action that the
+ * client can make e.g., login, register, or sendMessage.
+ * It is recommended if you want to get data from the server to use a DataBox instead of a
+ * controller because it is much easier to use and provides the
+ * functionality to keep the data up to date in real time.
+ */
 export default class Controller {
     /**
      * @description
