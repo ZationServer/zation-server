26401f2ce93bcc4b08d3bcf73ad54702ba0dc820
Luca Scaringella <lucalstyle@gmail.com>
2019-07-01 13:04:51 +0200

    Added DataBox API methods

--- src/lib/api/dataBox/DataBox.ts
+++ src/lib/api/dataBox/DataBox.ts
@@ -28,7 +28,8 @@ export default class DataBox extends DataBoxCore {
     }
 
     /**
-     * This method is used to get the current data or more data of the data box.
+     * **Can be overridden.**
+     * This method is used to get the current data or more data of the DataBox.
      * You usually request your database and return the data, and if no more data is available,
      * you should throw a NoMoreDataAvailableError or call the internal noMoreDataAvailable method.
      * A client can call that method multiple times.
@@ -46,6 +47,57 @@ export default class DataBox extends DataBoxCore {
      */
     protected getData(indicator : number,sessionData : object){
         this.noMoreDataAvailable();
+    }
+
+    /**
+     * **Not override this method.**
+     * The close function will close the DataBox for every client on every server.
+     * You optionally can provide a code or any other information for the client.
+     * Usually, the close function is used when the data is completely deleted from the system.
+     * For example, a chat that doesn't exist anymore.
+     * @param code
+     * @param data
+     */
+    close(code : number,data : any){
+    }
+
+    /**
+     * **Not override this method.**
+     * The reload function will force all clients of the DataBox to reload the data.
+     * This method is used internally if it was detected that a worker had
+     * missed a cud (create, update, or delete) operation.
+     * @param forEveryServer
+     */
+    doReload(forEveryServer : boolean = false){
+    }
+
+    /**
+     * **Can be overridden.**
+     * A function that gets triggered before an insert into the DataBox.
+     * Can be used to insert the data in the database.
+     * @param keyPath
+     * @param value
+     */
+    protected async beforeInsert(keyPath : string[],value : any) {
+    }
+
+    /**
+     * **Can be overridden.**
+     * A function that gets triggered before an update of data in the DataBox.
+     * Can be used to update the data in the database.
+     * @param keyPath
+     * @param value
+     */
+    protected async beforeUpdate(keyPath : string[],value : any) {
+    }
+
+    /**
+     * **Can be overridden.**
+     * A function that gets triggered before a delete of data in the DataBox.
+     * Can be used to delete the data in the database.
+     * @param keyPath
+     */
+    protected async beforeDelete(keyPath : string[]) {
     }
 }
 
--- src/lib/api/dataBox/DataIdBox.ts
+++ src/lib/api/dataBox/DataIdBox.ts
@@ -34,7 +34,8 @@ export default class DataIdBox extends DataBoxCore {
     }
 
     /**
-     * This method is used to get the current data or more data of the data box.
+     * **Can be overridden.**
+     * This method is used to get the current data or more data of the DataBox.
      * You usually request your database and return the data, and if no more data is available,
      * you should throw a NoMoreDataAvailableError or call the internal noMoreDataAvailable method.
      * A client can call that method multiple times.
@@ -58,7 +59,8 @@ export default class DataIdBox extends DataBoxCore {
 
     // noinspection JSUnusedGlobalSymbols
     /**
-     * Check if the id is valid for this data box.
+     * **Can be overridden.**
+     * Check if the id is valid for this DataBox.
      * To block the id, you only need to return an object (that can be error information) or false.
      * If you want to allow the id, you have to return nothing or a true.
      * @param id
@@ -67,6 +69,61 @@ export default class DataIdBox extends DataBoxCore {
     public isIdValid(id : string,smallBag : SmallBag) : Promise<boolean | Record<string,any> | void> | boolean | Record<string,any> | void {
     }
 
+    /**
+     * **Not override this method.**
+     * The close function will close the DataBox with the id for every client on every server.
+     * You optionally can provide a code or any other information for the client.
+     * Usually, the close function is used when the data is completely deleted from the system.
+     * For example, a chat that doesn't exist anymore.
+     * @param id
+     * @param code
+     * @param data
+     */
+    close(id : string,code : number,data : any){
+    }
+
+    /**
+     * **Not override this method.**
+     * The reload function will force all clients of the DataBox to reload the data.
+     * This method is used internally if it was detected that a worker had
+     * missed a cud (create, update, or delete) operation.
+     * @param id
+     * @param forEveryServer
+     */
+    doReload(id : string,forEveryServer : boolean = false){
+    }
+
+    /**
+     * **Can be overridden.**
+     * A function that gets triggered before an insert into the DataBox.
+     * Can be used to insert the data in the database.
+     * @param id
+     * @param keyPath
+     * @param value
+     */
+    protected async beforeInsert(id : string,keyPath : string[],value : any) {
+    }
+
+    /**
+     * **Can be overridden.**
+     * A function that gets triggered before an update of data in the DataBox.
+     * Can be used to update the data in the database.
+     * @param id
+     * @param keyPath
+     * @param value
+     */
+    protected async beforeUpdate(id : string,keyPath : string[],value : any) {
+    }
+
+    /**
+     * **Can be overridden.**
+     * A function that gets triggered before a delete of data in the DataBox.
+     * Can be used to delete the data in the database.
+     * @param id
+     * @param keyPath
+     */
+    protected async beforeDelete(id : string,keyPath : string[]) {
+    }
 }
 
 export interface DataIdBoxClass {
