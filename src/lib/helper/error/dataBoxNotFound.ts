2f0aee8ec45046c57c2337161a3a6e34c12ebb55
Luca Scaringella <lucalstyle@gmail.com>
2019-07-16 15:46:27 +0200

    Create dataBox not found error.

---
+++ src/lib/helper/error/dataBoxNotFound.ts
@@ -0,0 +1,24 @@
+/*
+Author: Luca Scaringella
+GitHub: LucaCode
+©Copyright by Luca Scaringella
+ */
+
+import CodeError        from "./codeError";
+import {MainBackErrors} from "../zationBackErrors/mainBackErrors";
+
+export default class DataBoxNotFound extends CodeError
+{
+    private readonly className : string;
+
+    constructor(className : string)
+    {
+        super(MainBackErrors.dataBoxNotFound,{className : className});
+        this.className = className;
+    }
+
+    // noinspection JSUnusedGlobalSymbols
+    getClassName(): string {
+        return this.className;
+    }
+}
