e0ebeeac6707a966a351914562d2eb41c50f4545
Luca Scaringella <lucalstyle@gmail.com>
2019-07-18 13:09:10 +0200

    Added no more data available error name.

--- src/lib/helper/dataBox/noMoreDataAvailable.ts
+++ src/lib/helper/dataBox/noMoreDataAvailable.ts
@@ -4,9 +4,12 @@ GitHub: LucaCode
 ©Copyright by Luca Scaringella
  */
 
+import {ErrorName} from "../constants/errorName";
+
 export default class NoMoreDataAvailableError extends Error
 {
     constructor() {
         super();
+        this.name = ErrorName.NO_MORE_DATA_AVAILABLE;
     }
 }
