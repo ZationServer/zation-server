e4be86e2a8f23f5b8225d860143a4d57a229b6f3
Luca Scaringella <lucalstyle@gmail.com>
2019-08-17 03:56:33 +0200

    Update to version 1.2.2

--- src/lib/core/zationMaster.ts
+++ src/lib/core/zationMaster.ts
@@ -27,7 +27,7 @@ import ConfigLoader            from "../main/config/manager/configLoader";
 
 export default class ZationMaster {
     private static instance: ZationMaster | null = null;
-    private static readonly version: string = '1.2.1';
+    private static readonly version: string = '1.2.2';
 
     private readonly serverStartedTimeStamp: number;
     private zc: ZationConfigMaster;
