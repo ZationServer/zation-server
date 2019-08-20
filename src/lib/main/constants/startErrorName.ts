e79dd507f7091ab8f1739e67c01626b84dd6c427
Luca Scaringella <lucalstyle@gmail.com>
2019-08-20 22:05:18 +0200

    Update startErrorName.ts

--- src/lib/main/constants/startErrorName.ts
+++ src/lib/main/constants/startErrorName.ts
@@ -8,6 +8,6 @@ export enum StartErrorName {
     CONFIG_ERRORS               = 'CONFIG_ERRORS',
     PORT_IN_USE                 = 'PORT_IN_USE',
     LOAD_UWS_FAILED             = 'LOAD_UWS_FAILED',
-    REGISTER_TO_STATE_SERVER_FAILED = ''
+    REGISTER_TO_STATE_SERVER_FAILED = 'REGISTER_TO_STATE_SERVER_FAILED'
 }
 
