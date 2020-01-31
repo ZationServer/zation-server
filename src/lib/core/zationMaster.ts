8e477e0210dad6e965eeebdf7b5c8eac4cb98a58
Luca Scaringella <lucalstyle@gmail.com>
2020-01-31 15:54:42 +0100

    Update to version 1.4.4

--- src/lib/core/zationMaster.ts
+++ src/lib/core/zationMaster.ts
@@ -29,7 +29,7 @@ import ConfigLoader            from "../main/config/manager/configLoader";
 
 export default class ZationMaster {
     private static instance: ZationMaster | null = null;
-    private static readonly version: string = '1.4.3';
+    private static readonly version: string = '1.4.4';
     static readonly minLicenseVersionRequired: number = 1;
 
     private readonly serverStartedTimeStamp: number;
