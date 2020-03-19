2a80157aceae35c4a360c8eb531ff6ec9753b127
Luca Scaringella <lucalstyle@gmail.com>
2020-03-19 22:41:09 +0100

    Update to version 1.5.3

--- src/lib/core/zationMaster.ts
+++ src/lib/core/zationMaster.ts
@@ -37,7 +37,7 @@ global[processTypeSymbol] = ProcessType.Master;
 
 export default class ZationMaster {
     private static instance: ZationMaster | null = null;
-    private static readonly version: string = '1.5.2';
+    private static readonly version: string = '1.5.3';
     static readonly minLicenseVersionRequired: number = 1;
 
     private readonly serverStartedTimeStamp: number;
