5b069a7d9f182887393a52b457538fb1af8ba195
Luca Scaringella <lucalstyle@gmail.com>
2020-04-30 23:01:36 +0200

    Improve Register decorator. Improve class name parsing.

--- src/lib/api/decorator/component/Register.ts
+++ src/lib/api/decorator/component/Register.ts
@@ -10,67 +10,50 @@ import Controller       from "../../Controller";
 import Databox          from "../../databox/Databox";
 import DataboxFamily    from "../../databox/DataboxFamily";
 import ConfigBuildError from "../../../main/config/manager/configBuildError";
-import Router           from '../../Router';
+import {parseComponentClassName} from '../../../main/utils/componentUtils';
 
-type RegisterDecorator = {
-    (target: Component): void,
-    /**
-     * Specifies the API level of the component.
-     * @param apiLevel
-     */
-    withApiLevel(apiLevel?: number): RegisterDecorator;
-    /**
-     * Registers the component with a specific router.
-     * @param router
-     */
-    withRouter(router?: Router): RegisterDecorator;
-};
 /**
  * Register a component (Controller or Databox).
  * You only have to import the file in the app config.
- * This decorator provides a fluent API to specify the API level, router,
- * or if it should register an auth controller.
- * You are able to register multiple components with the same identifier but different API levels.
- * The identifier of the component will be created with the routes and the provided name.
- * @param name
- * If not provided, it takes the class name.
- * Notice that "Controller" and "Databox" will be removed from the class name.
- * To specify more classes with different versions but the same name, you can use the '$' sign.
- * All characters behind the $ sign will be removed.
+ * You are able to register multiple components with the same
+ * identifier but different API levels.
+ * The identifier of the component will be created with the name of the component.
+ * The register decorator parses the name and API level from the class name.
+ * You need to follow a specific name convention, see the code examples.
+ * But you are able to override the parsed name and API level
+ * with the first parameter of this decorator.
+ * @example
+ * Example 1:
+ * class SendMessageController extends Controller {}
+ * Parsed: name = sendMessage, apiLevel = undefined
+ *
+ * Example 2:
+ * class BlockUserController_4 extends Controller {}
+ * Parsed: name = blockUser, apiLevel = 4
+ *
+ * Example 3:
+ * class ProfileDatabox_13 extends Controller {}
+ * Parsed: name = profile, apiLevel = 13
+ * @param override
+ * The parameter to override the parsed name and API level from the class name.
  */
-export const Register = (name?: string): RegisterDecorator => {
-    let targetApiLevel: number | undefined = undefined;
-    let targetRouter: Router | undefined = undefined;
-
-    const func = (target: Component) => {
-        let targetName;
-        if(name === undefined) {
-            let className = target.name;
-            const indexOfDollar = className.indexOf("$");
-            if(indexOfDollar !== -1){
-                className = className.slice(0,indexOfDollar);
-            }
-            targetName = className.replace(/Controller|Databox/g, '');
-        }
-        else {
-            targetName = name;
-        }
+export const Register = (override: {name?: string, apiLevel?: number} = {}): (target: Component) => void => {
+    return (target: Component) => {
         if(target.prototype instanceof Controller || target.prototype instanceof Databox
             || target.prototype instanceof DataboxFamily){
-            targetRouter ? targetRouter.register(targetName,target,targetApiLevel) :
-                Config.registerComponent(targetName,target,targetApiLevel);
+
+            let {name,apiLevel} = parseComponentClassName(target.name);
+            if(override.name !== undefined){
+                name = override.name;
+            }
+            if(override.apiLevel !== undefined){
+                apiLevel = parseInt(override.apiLevel as any);
+            }
+
+            Config.registerComponent(name,target,apiLevel);
         }
         else {
             throw new ConfigBuildError(`The register decorator can only be used on classes that extend the Controller, Databox or DataboxFamily class.`);
         }
     };
-    func.withApiLevel = (apiLevel?: number) => {
-        targetApiLevel = apiLevel;
-        return func;
-    };
-    func.withRouter = (router?: Router) => {
-        targetRouter = router;
-        return func;
-    };
-    return func;
 };
---
+++ src/lib/main/utils/componentUtils.ts
@@ -0,0 +1,40 @@
+/*
+Author: Luca Scaringella
+GitHub: LucaCode
+Copyright(c) Luca Scaringella
+ */
+
+import ConfigBuildError from '../config/manager/configBuildError';
+
+/**
+ * Parse the API level and name from a component class name.
+ * @param name
+ */
+export function parseComponentClassName(name: string): {name: string,apiLevel?: number}
+{
+    const typeIndex = Math.max(name.indexOf('Controller'),name.indexOf('Databox'));
+    if(typeIndex <= 0){
+        throw new ConfigBuildError(`Cannot parse component class name: '${name}'. Check syntax.`);
+    }
+
+    let resName = name.slice(0,typeIndex);
+    resName = resName.charAt(0).toLowerCase() + resName.slice(1);
+
+
+    let resApiLevel;
+    const versionSplitterIndex = name.indexOf('_',typeIndex);
+    if(versionSplitterIndex !== -1){
+        if((versionSplitterIndex + 1) < name.length){
+            resApiLevel = parseInt(name.slice(versionSplitterIndex + 1));
+        }
+        else {
+            throw new ConfigBuildError(`Cannot parse component class name: '${name
+            }'. API level not defined but marked with an underscore. Check syntax.`);
+        }
+    }
+
+    return {
+        name: resName,
+        apiLevel: resApiLevel
+    }
+}
