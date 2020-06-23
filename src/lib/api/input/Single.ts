28ab5b204324575a592bd65c3364307b7670cadf
Luca Scaringella <lucalstyle@gmail.com>
2020-06-23 16:36:00 +0200

    Improve model single function.

--- src/lib/api/input/Single.ts
+++ src/lib/api/input/Single.ts
@@ -4,12 +4,12 @@ GitHub: LucaCode
 Copyright(c) Luca Scaringella
  */
 
-import {Model, SingleModelInput} from '../../main/config/definitions/parts/inputConfig';
+import {Model, ModelConfig} from '../../main/config/definitions/parts/inputConfig';
 
 /**
  * Marks a disposable model as a single input model.
  * @param model
  */
-export function $single(model: Model): SingleModelInput {
+export function $single<T extends Model | ModelConfig>(model: T): [T] {
     return [model];
 }
