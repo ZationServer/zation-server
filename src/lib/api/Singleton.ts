7ad9338e3c3be29a1225c73acf25f4476023d8d2
Luca Scaringella <lucalstyle@gmail.com>
2020-07-20 03:15:41 +0200

    Update ts doc.

--- src/lib/api/Singleton.ts
+++ src/lib/api/Singleton.ts
@@ -13,6 +13,7 @@ export const singletonSymbol = Symbol();
  * The Singleton decorator can be used to mark a class as a singleton.
  * Zation will create the singleton instance internally and you
  * can access it with the bag get method or inject it with the inject decorator.
+ * Notice that the constructor should not take any arguments.
  * It is also possible to mark a non-static method with the initializer decorator.
  * By that, this method will be called with a this-context that is bound
  * to the singleton instance.
