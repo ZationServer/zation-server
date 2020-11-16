30c2cb5aad83f860ce10354507f698da0b33f90c
Luca Scaringella <lucalstyle@gmail.com>
2020-11-16 22:15:57 +0100

    Update to version 1.7.8

--- src/lib/core/zationMaster.ts
+++ src/lib/core/zationMaster.ts
@@ -42,7 +42,7 @@ const  SocketCluster: any = require('socketcluster');
 
 export default class ZationMaster {
     private static instance: ZationMaster | null = null;
-    private static readonly version: string = '1.7.7';
+    private static readonly version: string = '1.7.8';
     static readonly minLicenseVersionRequired: number = 1;
 
     private readonly serverStartedTimeStamp: number;
